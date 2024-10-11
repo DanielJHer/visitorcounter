8939bbe4cb5989797f1eefa541c8c4518b886ea5
Daniel Her <danieljhher@gmail.com>
2024-10-10 21:09:29 -0700

    added testing

---
+++ main.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+  // API endpoint URL
+  const apiGatewayURL =
+    'https://iehktaw4th.execute-api.us-east-1.amazonaws.com/default/visitorcount';
+
+  // Fetch visitor count from Lambda function via API Gateway
+  fetch(apiGatewayURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Update the visitor count on the webpage with the updated value
+      document.getElementById('visitor-count').textContent = data.visitor_count;
+    })
+    .catch((error) => {
+      console.log('Error', error);
+      document.getElementById('visitor-count').textContent =
+        'Error loading visitor count.';
+    });
+};
